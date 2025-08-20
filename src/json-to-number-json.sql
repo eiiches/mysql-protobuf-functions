e6f9b54889a47470571098382521508bd49a600d
Eiichi Sato <sato.eiichi@gmail.com>
2025-08-20 14:05:43 +0900

    . json_to_number_json null for proto3 fields.

--- src/json-to-number-json.sql
+++ src/json-to-number-json.sql
@@ -647,7 +647,7 @@ BEGIN
 		SET field_type = JSON_EXTRACT(field_descriptor, '$."5"'); -- type
 		SET field_type_name = JSON_UNQUOTE(JSON_EXTRACT(field_descriptor, '$."6"')); -- type_name
 		SET json_name = JSON_UNQUOTE(JSON_EXTRACT(field_descriptor, '$."10"')); -- json_name
-		SET proto3_optional = CAST(JSON_EXTRACT(field_descriptor, '$."17"') AS UNSIGNED) = 1; -- proto3_optional
+		SET proto3_optional = CAST(COALESCE(JSON_EXTRACT(field_descriptor, '$."17"'), 0) AS UNSIGNED) = 1; -- proto3_optional
 		SET oneof_index = JSON_EXTRACT(field_descriptor, '$."9"'); -- oneof_index
 
 		SET is_repeated = (field_label = 3);
@@ -771,7 +771,9 @@ BEGIN
 			-- Explicit JSON null for a field that has field presence tracking means the field is not set, except
 			-- if the field is .google.protobuf.Value. Explicit JSON null for a Value field, should be recognized
 			-- as a non-null Value message with kind.null_value set to NULL_VALUE.
-			IF has_presence AND JSON_TYPE(field_json_value) = 'NULL' AND (field_type IS NULL OR field_type <> '.google.protobuf.Value') THEN
+			-- Explicit JSON null for a field that doesn't have a field presence tracking (>=proto3) means a zero
+			-- value is set (and zero values are omitted in ProtoNumberJSON or on wire).
+			IF JSON_TYPE(field_json_value) = 'NULL' AND (field_type IS NULL OR field_type <> '.google.protobuf.Value') THEN
 				SET field_index = field_index + 1;
 				ITERATE field_loop;
 			END IF;
